caaebee01625bc59d8d78948b43e74e81b1f8b7c
suresh_b1 <suresh.b1@photoninfotech.net>
2019-03-11 21:40:35 +0530

    Public folder created with html and favicon

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from  'react';
 import ReactDOM from 'react-dom';
-import SS from './style.css';
-
-const App = () => (
-    <div>
-        <h1>Hello Developer, You did awasome job.</h1>
-    </div>
-);
-
+import App from './App';
+import './style.css';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
