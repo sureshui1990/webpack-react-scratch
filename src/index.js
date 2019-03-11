03a9890568782fb090cfac88d5d3725b86d17b59
suresh_b1 <suresh.b1@photoninfotech.net>
2019-03-11 20:56:56 +0530

    webpack server working, localhost:8080 is available with build feature

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from  'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <div>
+        <h1>Hello Developer, You did awasome job.</h1>
+    </div>
+);
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
